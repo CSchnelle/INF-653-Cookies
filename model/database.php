7cef7592a9d5f237f07b65d81c3901aa32fcaa89
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-31 05:29:40 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,25 +1,13 @@
 <?php
-    //local development server connection
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    //$password = 'pa55word';
-
-    // Heroku connection
-    
     $dsn = 'mysql:host=zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=kzi9vdecvlvgv17f';
     $username = 'dg9aqktj30j1gcwb';
     $password = 've0htf4ekmzirmi3';
-    
-    try {
-        //local development server connection
-        //if using a $password, add it as 3rd parameter
-        $db = new PDO($dsn, $username);
-
-        // Heroku connection
-        //$db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('../errors/database_error.php');
-        exit();
-    }
+ 
+try {
+  $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+  $error_message = $e->getMessage();
+  include('database_error.php');
+  exit();
+}
 ?>
