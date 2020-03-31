737de316fce73f744457b732f4ea6920dac254cc
Cat Schnelle <40611881+CSchnelle@users.noreply.github.com>
2020-03-31 05:27:30 -0500

    test

--- model/database.php
+++ model/database.php
@@ -5,11 +5,11 @@
     //$password = 'pa55word';
 
     // Heroku connection
-    /*
-    $dsn = 'mysql:host=AVeryLongURLprovidedforJawsDBhost;dbname=YourJawsDBdbname';
-    $username = 'Your JawsDB username';
-    $password = 'Your JawsDB password';
-    */
+    
+    $dsn = 'mysql:host=zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=kzi9vdecvlvgv17f';
+    $username = 'dg9aqktj30j1gcwb';
+    $password = 've0htf4ekmzirmi3';
+    
     try {
         //local development server connection
         //if using a $password, add it as 3rd parameter
